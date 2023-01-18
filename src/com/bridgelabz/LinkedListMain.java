8956d833a05f79a39e87445b697eee998f79f51c
Your Name <you@example.com>
2023-01-18 09:09:34 +0530

    Added: Add 70 then 36 then 50

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,9 +5,9 @@ public class LinkedListMain {
     public static void main(String[] args) {
 
         LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.push(56);
-        linkedList.push(30);
         linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
         linkedList.display();
     }
 }
